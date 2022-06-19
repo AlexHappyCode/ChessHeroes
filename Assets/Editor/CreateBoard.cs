cdd97c359c4b779489dcbf84d51da11956b6b556
Alexander Pena <arpenacoding@gmail.com>
2022-06-19 16:07:05 -0700

    refactored createboard out of editor

--- Assets/Editor/CreateBoard.cs
+++ Assets/Editor/CreateBoard.cs
@@ -10,14 +10,18 @@ public class CreateBoard : Editor {
         var board = (Board)target;
         
         if (GUILayout.Button("Create Board")) {
+            board.CreateBoard();
             //Debug.Log("test");
-            for (int i = 0; i < 8; i++) {
-                for (int j = 0; j < 8; j++) {
+            /*
+            for (int x = 0; x < 8; x++) {
+                for (int z = 0; z < 8; z++) {
                     var newTile = Instantiate(board.tile, board.transform);
-                    newTile.transform.position += new Vector3(j, 0, i);
+                    newTile.transform.position += new Vector3(z, 0, x);
                     newTile.name = "tile " + newTile.transform.position.z + " " + newTile.transform.position.x;
+                    tiles[x, z] = newTile;
                 }
             }
+            */
         } else if (GUILayout.Button("Empty Board")) {
             //Debug.Log("childCount: " + board.transform.childCount);
             
--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -6,5 +6,49 @@ using UnityEngine.Rendering.Universal.Internal;
 public class Board : MonoBehaviour {
     [SerializeField]
     public GameObject tile;
+
+    //private GameObject[,] tiles = new GameObject[TILE_COUNT_Z, TILE_COUNT_X];
+    List<List<GameObject>> tiles = new List<List<GameObject>>();  //Create 2d List
+
+    private Camera currentCamera;
+
+    private const int TILE_COUNT_X = 8;
+    private const int TILE_COUNT_Z = 8;
+
+    public void CreateBoard() {
+        for (int z = 0; z < TILE_COUNT_Z; z++) {
+            tiles.Add(new List<GameObject>());
+            for (int x = 0; x < TILE_COUNT_X; x++) {
+                var newTile = Instantiate(tile, transform);
+                newTile.transform.position += new Vector3(x, 0, z);
+                newTile.name = "tile " + newTile.transform.position.z + " " + newTile.transform.position.x;
+                tiles[z].Add(newTile);
+            }
+        }
+    }
+
+    public void update() {
+        if (!currentCamera) {
+            currentCamera = Camera.current;
+            return;
+        }
+
+        RaycastHit info;
+        Ray ray = currentCamera.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out info, 100, LayerMask.GetMask("Tile"))) {
+            Vector2Int hitPosition = LookupTileIndex(info.transform.gameObject);
+        }
+    }
+
+    private Vector2Int LookupTileIndex(GameObject hitInfo) {
+        for (int x = 0; x < TILE_COUNT_X; x++) {
+            for (int z = 0; z < TILE_COUNT_Z; z++) {
+                if (tiles[x][z] == hitInfo) {
+                    return new Vector2Int(x, z);
+                }
+            }
+        }
+        return -Vector2Int.one; // Invalid
+    }
 }
    
