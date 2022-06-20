48cb28ff647093e343d08f389b1d7bc59af3df7c
Alexander Pena <arpenacoding@gmail.com>
2022-06-19 20:55:18 -0700

    got a sweet ray to shoot out of the mouse

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -11,6 +11,7 @@ public class Board : MonoBehaviour {
     List<List<GameObject>> tiles = new List<List<GameObject>>();  //Create 2d List
 
     private Camera currentCamera;
+    private Vector2Int currentHover;
 
     private const int TILE_COUNT_X = 8;
     private const int TILE_COUNT_Z = 8;
@@ -27,25 +28,40 @@ public class Board : MonoBehaviour {
         }
     }
 
-    public void update() {
+    public void Update() {
         if (!currentCamera) {
-            currentCamera = Camera.current;
+            currentCamera = Camera.main;
             return;
         }
 
         RaycastHit info;
         Ray ray = currentCamera.ScreenPointToRay(Input.mousePosition);
+        // this is cool as shit
+        Debug.DrawRay(ray.origin, ray.direction * 10, Color.yellow);
         if (Physics.Raycast(ray, out info, 100, LayerMask.GetMask("Tile"))) {
-            Vector2Int hitPosition = LookupTileIndex(info.transform.gameObject);
+            //Vector2Int hitPosition = LookupTileIndex(info.transform.gameObject);
+            var hitPosition = new Vector2Int(0, 0);
+            if (currentHover == -Vector2Int.one) {
+                currentHover = hitPosition;
+                //tiles[hitPosition.y][hitPosition.x].layer = LayerMask.NameToLayer("Hover");
+            }
+            if (currentHover != -hitPosition) {
+                //tiles[currentHover.y][currentHover.x].layer = LayerMask.NameToLayer("Tile");
+                currentHover = hitPosition; 
+                //tiles[hitPosition.y][hitPosition.x].layer = LayerMask.NameToLayer("Hover");
+            }
+        }
+        else {
+            if (currentHover != -Vector2Int.one) { 
+                //tiles[currentHover.y][currentHover.x].layer = LayerMask.NameToLayer("Tile");
+            }
         }
     }
-
     private Vector2Int LookupTileIndex(GameObject hitInfo) {
-        for (int x = 0; x < TILE_COUNT_X; x++) {
-            for (int z = 0; z < TILE_COUNT_Z; z++) {
-                if (tiles[x][z] == hitInfo) {
+        for (int z = 0; z < TILE_COUNT_Z; z++) {
+            for (int x = 0; x < TILE_COUNT_X; x++) {
+                if (tiles[z][x] == hitInfo)
                     return new Vector2Int(x, z);
-                }
             }
         }
         return -Vector2Int.one; // Invalid
